ab602522c514441999db2f314d4f87843ace3ee9
35277Avishkar <119780684+35277Avishkar@users.noreply.github.com>
2023-03-01 15:40:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import pandas as pd
 import numpy as np
 df = pd.read_csv('iris_dataset.csv')
+# Add null values to the file 
+# df.loc[5, 'species'] = ''
+# df.to_csv("iris_dataset.csv", index=False)
+# Check for null values
+bool_series = pd.isnull(df["species","sepal_lenght"])
+df[bool_series]
 print(df.to_string())
